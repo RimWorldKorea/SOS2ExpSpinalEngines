eb01445322ccc07ffc5edb38ac5f357c42cd1b7f
TheCafFiend <github@kirax.net>
2023-12-16 10:18:01 -0700

    Add project files.

---
+++ Source/CompProperties_EngineTrailExtended.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Verse;
+
+namespace RimWorld
+{
+    public class CompProperties_EngineTrailExtended : CompProperties_EngineTrail
+    {
+        public CompProperties_EngineTrailExtended()
+        {
+            this.compClass = typeof(CompEngineTrailExtended);
+        }
+    }
+}
+
