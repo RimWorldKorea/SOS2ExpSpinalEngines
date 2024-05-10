c7a1373bfde39118826043c0b4f9d5ac1a252f10
TheCafFiend <github@kirax.net>
2024-05-10 17:32:55 -0600

    Removed fuel (pending SOS2 change), uses XML directly now.
    Cleaned up some weirdo edge cases, it turns out the shipcache wasn't really built for a multiblock, who knew?

--- Source/CompProperties_SpinalEngineTrail.cs
+++ Source/CompProperties_SpinalEngineTrail.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using Verse;
-using TheCafFiend;
+﻿using Verse;
 
 namespace TheCafFiend
 {
     public class CompProperties_SpinalEngineTrail : SaveOurShip2.CompProps_EngineTrail
     {
+        int baseFuelUse = 0;
         public CompProperties_SpinalEngineTrail()
         {
             this.compClass = typeof(CompSpinalEngineTrail);
